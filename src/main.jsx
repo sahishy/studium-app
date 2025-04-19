06f32c7181290effe0988e749a9b7d453bab694b
Sahish Durgam <91765380+sahishy@users.noreply.github.com>
2025-04-19 14:45:48 -0400

    switch to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router  } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<BrowserRouter basename={import.meta.env.BASE_URL}>
+		<Router>
 			<AuthProvider>
 				<App/>
 			</AuthProvider>
-		</BrowserRouter>
+		</Router>
 	</StrictMode>,
 )
