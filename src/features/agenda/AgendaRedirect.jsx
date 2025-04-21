07d504cddddca586eadee66fffa99af5e3386141
Sahish Durgam <91765380+sahishy@users.noreply.github.com>
2025-04-21 17:56:38 -0400

    fix agenda page cache bug

--- src/features/agenda/AgendaRedirect.jsx
+++ src/features/agenda/AgendaRedirect.jsx
@@ -2,9 +2,14 @@ import { Navigate } from 'react-router-dom';
 
 const AgendaIndexRedirect = () => {
 
-    const last = localStorage.getItem('agenda:lastTab');
+    const pages = ['list', 'calendar', 'board']
 
-    const defaultTab = last ? last : 'overview'
+    const lastPage = localStorage.getItem('agenda:lastTab');
+    if(lastPage && !pages.includes(lastPage)) {
+        lastPage = 'list';
+    }
+
+    const defaultTab = lastPage ? lastPage : 'list'
 
     return <Navigate to={defaultTab} replace/>
 
