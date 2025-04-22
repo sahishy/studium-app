a809dcc26809fb2d39b68e14504aabab85fee98e
Sahish Durgam <91765380+sahishy@users.noreply.github.com>
2025-04-21 21:09:51 -0400

    undo preview branch changes

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const isPreview = process.env.DEPLOY_TARGET === 'preview'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: isPreview ? '/studium-app/' : '/',
+  base: '/',
 })
